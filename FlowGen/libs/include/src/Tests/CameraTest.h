ec42b90b8b3e2ce59aa90e8f6febe9feb37219a5
PlayerZongying <playerzongying@gmail.com>
2024-11-28 12:36:45 +0100

    fix texture loader bug

--- FlowGen/libs/include/src/Tests/CameraTest.h
+++ FlowGen/libs/include/src/Tests/CameraTest.h
@@ -478,17 +478,17 @@ inline int CameraTest2()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
 
         //bind textures on corresponding texture units
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-        
-        std::cout<<texture1<<"\n"<<texture2<<"\n";
-
         // glActiveTexture(GL_TEXTURE0);
-        // glBindTexture(GL_TEXTURE_2D, t1->TextureObject);
+        // glBindTexture(GL_TEXTURE_2D, texture1);
         // glActiveTexture(GL_TEXTURE1);
-        // glBindTexture(GL_TEXTURE_2D, t2->TextureObject);
+        // glBindTexture(GL_TEXTURE_2D, texture2);
+        //
+        // std::cout<<texture1<<"\n"<<texture2<<"\n";
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, t1->TextureObject);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, t2->TextureObject);
         
         std::cout<<t1->TextureObject<<"\n"<<t2->TextureObject<<"\n";
 
